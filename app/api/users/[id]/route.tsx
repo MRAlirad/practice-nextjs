dc87310ffa1b7dfc69a1b69da743c90acdfbe378
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-03-18 00:03:41 +0330

    Updating an Object
    Building API
    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -12,3 +12,20 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 	if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
 	return NextResponse.json({ id: 1, name: 'Json' });
 }
+
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+	// Validate the request body
+	// If invalid, return 400
+	// fetch the user with given id
+	// If user does't exist, return 404
+	// Update the user
+	// Return the updated user
+
+	const body = await request.json();
+
+	if (!body.name) return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+
+	if (id > 10) return NextResponse.json({ error: 'User not Found' }, { status: 404 });
+
+	return NextResponse.json({ id: 1, name: body.name });
+}
