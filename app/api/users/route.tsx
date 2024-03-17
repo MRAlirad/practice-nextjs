7ea57a40fa373bb4387225618ea9b27da1ddf5e9
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-03-17 23:44:42 +0330

    Creating and Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,3 +1,4 @@
+import { error } from 'console';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function GET(request: NextRequest) {
@@ -7,4 +8,16 @@ export function GET(request: NextRequest) {
 		{ id: 1, name: 'Json' },
 		{ id: 2, name: 'John' },
 	]);
+}
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	// Validate
+	// if invalid, return 400
+	// Else, return
+	if (!body.name) return NextResponse.json({ error: 'Name is Required' }, { status: 400 });
+	return NextResponse.json({
+		id: 1,
+		name: body.name,
+	}, {status: 201});
 }
