1a62ce3b253da55e72ce335dd7824832ec1b1fc1
MRAlirad <m.r.alirad75@gmail.com>
2024-03-19 00:01:57 +0330

    Creating Data
    Database integration with Prisma
    Creating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -9,25 +9,49 @@ export async function GET(request: NextRequest) {
 	// 	{ id: 1, name: 'Json' },
 	// 	{ id: 2, name: 'John' },
 	// ]);
-	
+
 	//! Get users from DataBase
 	const users = await prisma.user.findMany();
 	return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
-	const body = await request.json();
+	// const body = await request.json();
 	// Validate
 	// if invalid, return 400
 	// Else, return
+	// const validation = schema.safeParse(body);
+	// if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
+	// if (!body.name) return NextResponse.json({ error: 'Name is Required' }, { status: 400 });
+	// return NextResponse.json(
+	// 	{
+	// 		id: 1,
+	// 		name: body.name,
+	// 	},
+	// 	{ status: 201 }
+	// );
+
+	//! Creating a User in DataBase
+	const body = await request.json();
+	
+	// Validate requested body
 	const validation = schema.safeParse(body);
 	if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
-	// if (!body.name) return NextResponse.json({ error: 'Name is Required' }, { status: 400 });
-	return NextResponse.json(
-		{
-			id: 1,
-			name: body.name,
+
+	// Check if email already exists
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email,
 		},
-		{ status: 201 }
-	);
+	});
+	if (user) return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+
+	// create new user
+	const newUser = await prisma.user.create({
+		data: {
+			name: body.name,
+			email: body.email,
+		},
+	});
+	return NextResponse.json(newUser, { status: 201 });
 }
