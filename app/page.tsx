e3e322f8604306de10f221fb52934ba14f0dfc40
MRAlirad <m.r.alirad75@gmail.com>
2024-03-19 23:52:44 +0330

    SEO
    Optimization
    SEO

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { authOptions } from './api/auth/[...nextauth]/route';
 import coffee from '@/public/next.svg';
+import { Metadata } from 'next';
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
@@ -11,7 +12,7 @@ export default async function Home() {
 
 	return (
 		<main>
-			<h1 className='font-poppins'>Hello {session && <span>{session.user!.name}</span>}</h1>
+			<h1 className="font-poppins">Hello {session && <span>{session.user!.name}</span>}</h1>
 			<Link href="/users">users</Link>
 			<Image
 				src="https://bit.ly/react-cover"
@@ -24,4 +25,13 @@ export default async function Home() {
 			/>
 		</main>
 	);
+}
+
+export async function generageMetadata() : Promise<Metadata> {
+	const product = await fetch('');
+	
+	return {
+		title: 'product.title',
+		description: 'product.description'
+	}
 }
