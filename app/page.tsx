c2bb7f8f7e73d1beabca9c7f77f50c0669dbcb58
MRAlirad <m.r.alirad75@gmail.com>
2024-03-19 22:43:18 +0330

    Optimizing Images
    Optimization
    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth';
 import Image from 'next/image';
 import Link from 'next/link';
 import { authOptions } from './api/auth/[...nextauth]/route';
+import coffee from '@/public/next.svg';
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
@@ -12,6 +13,15 @@ export default async function Home() {
 		<main>
 			<h1>Hello {session && <span>{session.user!.name}</span>}</h1>
 			<Link href="/users">users</Link>
+			<Image
+				src="https://bit.ly/react-cover"
+				alt="image"
+				width={300}
+				height={170}
+				// sizes="100vw"
+				// quality={100}
+				priority // for laxy loading images
+			/>
 		</main>
 	);
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // we need to register the domain so we allow to serve images and remote patterns for security reasons
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'bit.ly',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
