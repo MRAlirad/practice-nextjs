7c2c4e8a679422252dfde8abb02a841a7866a6eb
MRAlirad <m.r.alirad75@gmail.com>
2024-03-19 14:18:12 +0330

    Accessing Authentication Sessions on the Server
    Authentication
    Accessing Authentication Sessions on the Server

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,15 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
-const handler = NextAuth({
+export const authOptions = {
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
 		}),
 	],
-});
+}
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }; // any get or post request has sent to this endpoint will be handled inside this handler function
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import { getServerSession } from 'next-auth';
 import Image from 'next/image';
 import Link from 'next/link';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function Home() {
+export default async function Home() {
+	const session = await getServerSession(authOptions);
+	// this function works both in our pages as well as our route handlers
+	// if you have an API that needs to access the current session, in that API we can call this function
+
 	return (
 		<main>
-			<h1>Hello World</h1>
-			<Link href='/users'>users</Link>
+			<h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+			<Link href="/users">users</Link>
 		</main>
 	);
 }
