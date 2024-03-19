77a6b83e62ac8802ead696c3170640c09b18acae
MRAlirad <m.r.alirad75@gmail.com>
2024-03-19 13:29:41 +0330

    Configure Google Provider
    Authentication
    Configure Google Provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ const NavBar = () => {
         <div className='flex bg-slate-200 p-5'>
             <Link href='/' className='mr-5'> Nextjs</Link>
             <Link href='/users' className='mr-5'>Users</Link>
+            <Link href='/api/auth/signin' className='ml-auto'>Signin</Link>
         </div>
     )
 }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,13 @@
-import NextAuth from "next-auth/next";
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
 
-const handler = NextAuth({});
+const handler = NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+});
 
-export {handler as GET, handler as POST}; // any get or post request has sent to this endpoint will be handled inside this handler function
+export { handler as GET, handler as POST }; // any get or post request has sent to this endpoint will be handled inside this handler function
